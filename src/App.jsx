0c06062cad135b043d1293d6b68bd76f6e66b191
Mariana Correa <nana.filosofia@hotmail.com>
2023-03-15 18:16:07 -0300

    React terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import "./styles.css";
+
 
 
 function App() {
@@ -22,7 +24,7 @@ function App() {
       ...taskList,
       {
         id: Date.now(),
-        titulo: titulo,
+        title: title,
         categoria: categoria,
         data: data,
         descricao: descricao
@@ -30,35 +32,82 @@ function App() {
     ]);
     setId("")
     setTitle("");
+    setCategoria("");
     setDescricao("");
     setData("");
     
   }
 
+  function editItem(event){
+    event.preventDefault()
+
+    const copyTaskList = [...taskList]
+    const index = copyTaskList.findIndex((title)=>title.id === id)
+
+    copyTaskList[index].id = id
+    copyTaskList[index].title = title
+    copyTaskList[index].categoria = categoria
+    copyTaskList[index].data = data
+    copyTaskList[index].descricao = descricao
+
+    setTaskList(copyTaskList)
+
+    setId("")
+    setTitle("");
+    setCategoria("");
+    setDescricao("");
+    setData("");
+  }
+
   function apagarItem(id) {
-    
+    if (confirm("Clique em OK para confirmar a exclusão")) {
+      const result = taskList.filter((item)=> item.id !== id)
+      setTaskList(result)
+    }
+  }
+
+  function preencherEstados(item){
+    setTitle(item.title)
+    setCategoria(item.categoria)
+    setData(item.data)
+    setDescricao(item.descricao)
+    setId(item.id)
   }
 
   return (
-    <div>
-      <h1>Cadastrar Tarefa</h1>
-      <form onSubmit={addItem}>
+    <div className="container">
+      <div className="form">
+        <h1>Cadastrar Tarefa</h1>
+      <form  onSubmit={id ? editItem : addItem}>
         <input value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Título"/>
 
+        <br />
+        <br />
         
       <select value={categoria} onChange={(event) => setCategoria(event.target.value)} placeholder="Categoria">
+        <option value="" disabled>Selecione um valor:</option>
         <option value="trabalho">Trabalho</option>
         <option value="lazer">Lazer</option>
         <option value="prioridade">Prioridade</option>
         <option value="outros">Outros</option>
-        
       </select>
-      
-      <input value={data} onChange={(event) => setData(event.target.value)} placeholder="Data"/>
-      <input value={descricao} onChange={(event) => setDescricao(event.target.value)} placeholder="Descrição"/>
-      <input type="submit" value="Cadastrar" />
-      </form>
 
+      <br />
+      <br />
+
+      <input value={data} onChange={(event) => setData(event.target.value)} placeholder="Data"/>
+        <br />
+        <br />
+      <input value={descricao} onChange={(event) => setDescricao(event.target.value)} placeholder="Descrição"/>
+      <br />
+      <br />
+      <input type="submit" value={id ? "Salvar": "Cadastrar"} />
+      </form>
+      </div>
+      
+      <div className="tarefas">
+        <h2>Minhas Tarefas</h2>
+        <p>Total: {taskList.length} tarefas</p>
       {taskList.length > 0 ?(
         <ul>
           {taskList.map((item)=>{
@@ -70,12 +119,15 @@ function App() {
                 <p>{descricao}</p>
                 <p>{data}</p>
                 <button onClick={()=> apagarItem(item.id)}>Apagar</button>
+                <button onClick={()=> preencherEstados(item)} >Editar</button>
               </li>
             )
           })}
         </ul>) : (
           <p>Nenhuma tarefa cadastrada!</p>
         )}
+
+      </div>
     </div>
   )
 }
